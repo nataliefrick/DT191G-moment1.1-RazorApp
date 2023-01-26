24c615bf062ac5dcea80c295d10769242125ebb1
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2023-01-26 10:42:03 +0100

    Add project files.

---
+++ moment1/Pages/RazorPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace moment1.Pages
+{
+    public class RazorPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ moment1/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// Set active menu item.
+function SetActive(page) {
+    document.getElementById(page).classList.add("active");
+    console.log("click " + page);
+}
+
